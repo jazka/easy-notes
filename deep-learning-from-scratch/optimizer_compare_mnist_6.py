0f8273e0d64c345c70063f710cd0f5ba4bf52caf
Jia Zhikai <jiazhikai@testin.cn>
2019-07-24 00:45:55 +0800

    Add key answers for chapter 6

---
+++ deep-learning-from-scratch/optimizer_compare_mnist_6.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-06-23 00:01:03
+# @Author  : Jazka (jazka@testin.cn)
+# @Link    : http://www.testin.cn
+# @Version : $Id$
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dataset.mnist import load_mnist
+from common.multi_layer_net import MultiLayerNet
+from common.optimizer import *
+from common.util import smooth_curve
+
+def main():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+    train_size = x_train.shape[0]
+    batch_size = 128
+    max_iterations = 2000
+
+    optimizers = {}
+    optimizers['SGD'] = SGD()
+    optimizers['Momentum'] = Momentum()
+    optimizers['AdaGrad'] = AdaGrad()
+    optimizers['Adam'] = Adam()
+
+    networks = {}
+    train_loss = {}
+    for key in optimizers.keys():
+        networks[key] = MultiLayerNet(input_size=784, hidden_size_list=[100, 100, 100, 100],
+                                    output_size=10)
+        train_loss[key] = []
+
+    for i in range(max_iterations):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
+        for key in optimizers.keys():
+            grads = networks[key].gradient(x_batch, t_batch)
+            optimizers[key].update(networks[key].params, grads)
+
+            loss = networks[key].loss(x_batch, t_batch)
+            train_loss[key].append(loss)
+
+            if i % 100 == 0:
+                print( "===========" + "iteration:" + str(i) + "===========")
+                print(key + ":" + str(loss))
+
+    markers = {"SGD": "o", "Momentum": "x", "AdaGrad": "s", "Adam": "D"}
+    x = np.arange(max_iterations)
+    for key in optimizers.keys():
+        plt.plot(x, smooth_curve(train_loss[key]), marker=markers[key], markevery=100, label=key)
+    plt.xlabel("iterations")
+    plt.ylabel("loss")
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
